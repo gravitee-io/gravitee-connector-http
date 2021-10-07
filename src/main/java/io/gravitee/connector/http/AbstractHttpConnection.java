4e0cf33e242fac04720165d9e5d40beefb4d1d94
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2021-10-07 13:08:53 +0200

    feat(SME): add http connector implementation
    gravitee-io/issues#6132

---
+++ src/main/java/io/gravitee/connector/http/AbstractHttpConnection.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.connector.http;
+
+import io.gravitee.connector.api.Response;
+import io.gravitee.connector.http.endpoint.HttpEndpoint;
+import io.gravitee.gateway.api.handler.Handler;
+import io.vertx.core.http.HttpClient;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public abstract class AbstractHttpConnection<E extends HttpEndpoint> extends io.gravitee.connector.api.AbstractConnection {
+
+    protected final E endpoint;
+
+    public AbstractHttpConnection(E endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    public abstract void connect(
+        HttpClient httpClient,
+        int port,
+        String host,
+        String uri,
+        Handler<Void> connectionHandler,
+        Handler<Void> tracker
+    );
+
+    protected void sendToClient(Response response) {
+        this.responseHandler.handle(response);
+    }
+}
---
+++ src/main/java/io/gravitee/connector/http/grpc/GrpcConnector.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.connector.http.grpc;
+
+import io.gravitee.common.environment.Configuration;
+import io.gravitee.connector.api.EndpointException;
+import io.gravitee.connector.http.AbstractHttpConnector;
+import io.gravitee.gateway.api.proxy.ProxyRequest;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpVersion;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class GrpcConnector extends AbstractHttpConnector<GrpcEndpoint> {
+
+    public GrpcConnector(GrpcEndpoint endpoint, Configuration environment) {
+        super(endpoint, environment);
+    }
+
+    @Override
+    public HttpClientOptions createHttpClientOptions() throws EndpointException {
+        HttpClientOptions options = super.createHttpClientOptions();
+
+        // For GRPC, force HTTP/2 protocol
+        options.setProtocolVersion(HttpVersion.HTTP_2).setHttp2ClearTextUpgrade(false);
+
+        return options;
+    }
+
+    @Override
+    protected GrpcConnection create(ProxyRequest request) {
+        return new GrpcConnection(endpoint, request);
+    }
+}

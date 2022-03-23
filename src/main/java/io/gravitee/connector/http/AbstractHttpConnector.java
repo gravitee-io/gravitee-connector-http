2cd082f20acb1d1d5a41d2482c5eaba987dfa586
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2022-03-23 14:18:27 +0100

    fix: override headers in endpoint configuration
    gravitee-io/issues#7007

--- src/main/java/io/gravitee/connector/http/AbstractHttpConnector.java
+++ src/main/java/io/gravitee/connector/http/AbstractHttpConnector.java
@@ -91,7 +91,7 @@ public abstract class AbstractHttpConnector<E extends HttpEndpoint> extends Abst
         this.configuration = configuration;
     }
 
-    private final Map<Thread, HttpClient> httpClients = new ConcurrentHashMap<>();
+    protected final Map<Thread, HttpClient> httpClients = new ConcurrentHashMap<>();
 
     private final AtomicInteger requestTracker = new AtomicInteger(0);
 
@@ -122,7 +122,7 @@ public abstract class AbstractHttpConnector<E extends HttpEndpoint> extends Abst
                     .getHeaders()
                     .forEach(
                         header -> {
-                            request.headers().add(header.getName(), header.getValue());
+                            request.headers().set(header.getName(), header.getValue());
                         }
                     );
             }

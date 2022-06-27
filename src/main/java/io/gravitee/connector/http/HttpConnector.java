4c471bc0758794892ee8e60e7d4f73f1f6e7d7ce
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2022-06-27 18:22:58 +0200

    fix: manage HTTP1/HTTP2 headers conversion
    gravitee-io/issues#6719

--- src/main/java/io/gravitee/connector/http/HttpConnector.java
+++ src/main/java/io/gravitee/connector/http/HttpConnector.java
@@ -19,9 +19,12 @@ import io.gravitee.common.http.HttpHeaders;
 import io.gravitee.common.http.HttpMethod;
 import io.gravitee.connector.http.endpoint.HttpEndpoint;
 import io.gravitee.connector.http.ws.WebSocketConnection;
+import io.gravitee.gateway.api.http.HttpHeaderNames;
 import io.gravitee.gateway.api.proxy.ProxyRequest;
 import io.gravitee.node.api.configuration.Configuration;
 import io.netty.handler.codec.http.HttpHeaderValues;
+import java.net.URL;
+import java.util.Set;
 
 /**
  * @author David BRASSELY (david.brassely at graviteesource.com)
@@ -29,8 +32,21 @@ import io.netty.handler.codec.http.HttpHeaderValues;
  */
 public class HttpConnector extends AbstractHttpConnector<HttpEndpoint> {
 
+    protected static final Set<CharSequence> HTTP2_PSEUDO_HEADERS;
+
+    static {
+        HTTP2_PSEUDO_HEADERS = Set.of(Http2PseudoHeaderNames.AUTHORITY, Http2PseudoHeaderNames.PATH, Http2PseudoHeaderNames.SCHEME);
+    }
+
     public HttpConnector(HttpEndpoint endpoint, Configuration configuration) {
         super(endpoint, configuration);
+    }
+
+    @Override
+    protected void convertHeadersForHttpVersion(URL url, ProxyRequest request, String host) {
+        request.headers().set(HttpHeaderNames.HOST, host);
+        // Strip all HTTP/2 headers
+        HTTP2_PSEUDO_HEADERS.forEach(name -> request.headers().remove(name));
     }
 
     @Override
